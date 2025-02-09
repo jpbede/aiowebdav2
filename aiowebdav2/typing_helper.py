b5b4ffdaed0c042c21784fa4951c5b2872f1c307
Jan-Philipp Benecke <jan-philipp@bnck.me>
2025-02-09 21:05:47 +0100

    Improve typing of `typing_helper.py` (#3)

--- aiowebdav2/typing_helper.py
+++ aiowebdav2/typing_helper.py
@@ -5,6 +5,6 @@ from typing import Protocol
 class AsyncWriteBuffer(Protocol):
     """Protocol for async write buffer."""
 
-    async def write(self, data: bytes):
+    async def write(self, data: bytes) -> None:
         """Write data to buffer."""
         ...
