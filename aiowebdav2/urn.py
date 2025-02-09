af328dd4366ff3121a8dd460d8464af16523584b
Jan-Philipp Benecke <jan-philipp@bnck.me>
2025-02-09 21:03:19 +0100

    Improve typing of urn.py (#2)
    * Improve typing of `urn.py`
    
    * Improve typing of `urn.py`

--- aiowebdav2/urn.py
+++ aiowebdav2/urn.py
@@ -8,7 +8,7 @@ class Urn:
 
     separate = "/"
 
-    def __init__(self, path, directory=False):
+    def __init__(self, path: str, *, directory: bool = False) -> None:
         """Class for URN representation."""
         self._path = quote(path)
         expressions = r"/\.+/", "/+"
@@ -21,25 +21,25 @@ class Urn:
         if directory and not self._path.endswith(Urn.separate):
             self._path = f"{self._path}{Urn.separate}"
 
-    def __str__(self):
+    def __str__(self) -> str:
         """Return string representation of URN."""
         return self.path()
 
-    def path(self):
+    def path(self) -> str:
         """Return path."""
         return unquote(self._path)
 
-    def quote(self):
+    def quote(self) -> str:
         """Return quoted path."""
         return self._path
 
-    def filename(self):
+    def filename(self) -> str:
         """Return filename."""
         path_split = self._path.split(Urn.separate)
         name = path_split[-2] + Urn.separate if path_split[-1] == "" else path_split[-1]
         return unquote(name)
 
-    def parent(self):
+    def parent(self) -> str:
         """Return parent path."""
         path_split = self._path.split(Urn.separate)
         nesting_level = self.nesting_level()
@@ -53,22 +53,22 @@ class Urn:
             return unquote(parent + Urn.separate)
         return unquote(parent)
 
-    def nesting_level(self):
+    def nesting_level(self) -> int:
         """Return nesting level."""
         return self._path.count(Urn.separate, 0, -1)
 
-    def is_dir(self):
+    def is_dir(self) -> bool:
         """Return True if URN is directory."""
         return self._path[-1] == Urn.separate
 
     @staticmethod
-    def normalize_path(path):
+    def normalize_path(path: str) -> str:
         """Normalize path."""
         result = sub("/{2,}", "/", path)
         return result if len(result) < 1 or result[-1] != Urn.separate else result[:-1]
 
     @staticmethod
-    def compare_path(path_a, href):
+    def compare_path(path_a: str, href: str) -> bool:
         """Compare paths."""
-        unqouted_path = Urn.separate + unquote(urlsplit(href).path)
-        return Urn.normalize_path(path_a) == Urn.normalize_path(unqouted_path)
+        unquoted_path = Urn.separate + unquote(urlsplit(href).path)
+        return Urn.normalize_path(path_a) == Urn.normalize_path(unquoted_path)
