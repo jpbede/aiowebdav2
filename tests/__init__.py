d32c2b97d3fcf2de3ee86c766057e509461ab7ae
Jan-Philipp Benecke <jan-philipp@bnck.me>
2025-03-27 23:28:36 +0100

    Add test cases for `upload_iter` (#85)

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,6 @@
 """Helpers for the tests."""
 
+from collections.abc import AsyncGenerator
 from pathlib import Path
 
 
@@ -7,3 +8,9 @@ def load_responses(filename: str) -> str:
     """Load a responses."""
     path = Path(__file__).parent / "responses" / filename
     return path.read_text()
+
+
+async def upload_stream() -> AsyncGenerator[bytes, None]:
+    """Generate a stream of bytes."""
+    yield b"Hello, "
+    yield b"world!"
